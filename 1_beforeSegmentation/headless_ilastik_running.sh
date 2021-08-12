ebaef17466d07bdf74664ddf2567975626c3b254
ferperezv <frpvillatoro@gmail.com>
2021-08-12 12:51:38 +0300

    Updating version of headlless ilastik

--- 1_beforeSegmentation/headless_ilastik_running.sh
+++ 1_beforeSegmentation/headless_ilastik_running.sh
@@ -4,7 +4,7 @@
 ilastik="/home/fperez/apps/ilastik/ilastik-1.3.2post1-Linux/run_ilastik.sh"
 
 #ilastik project path
-ilastikfile="/mnt/d/users/fperez/NKI_TMAs_AF/Ilastik_labeling/Cellring_nuclei_background_Selected-Channels.ilp"
+ilastikfile="/mnt/d/users/fperez/NKI_TMAs_AF/Ilastik_labeling/Cellring_nuclei_background_Selected-Channels_3.ilp"
 
 #Project TMA path
 projectfolder="/mnt/d/users/fperez/NKI_TMAs_AF/"
@@ -13,7 +13,7 @@ projectfolder="/mnt/d/users/fperez/NKI_TMAs_AF/"
 dearrayfolder="dearray/selected_Channels/"
 
 #Output pixel probabilities folder
-probfolder="Probmaps_cyto_nuc_backg"
+probfolder="Probmaps_cyto_nuc_backg4"
 
 #Suffix for input images
 inputsuffix="*.tif"
@@ -23,7 +23,7 @@ outputsuffix="_Probabilities.tiff"
 
 
 #Running for each sample
-for samplename in `ls -d ${projectfolder}/TMA* | xargs -I {} basename {} | tail -n 7`
+for samplename in `ls -d ${projectfolder}/TMA* | xargs -I {} basename {}`
 do
 	inputimages=`ls ${projectfolder}/${samplename}/${dearrayfolder}/${inputsuffix}`
 
