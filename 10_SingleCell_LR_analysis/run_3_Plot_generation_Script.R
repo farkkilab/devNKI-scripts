cb360186845e6b74303409d365828acede7e3291
iamgayani <ananda.gaya.ac@gmail.com>
2025-03-26 14:02:49 +0200

    Added output dir

--- 10_SingleCell_LR_analysis/run_3_Plot_generation_Script.R
+++ 10_SingleCell_LR_analysis/run_3_Plot_generation_Script.R
@@ -1,9 +1,10 @@
 library(rmarkdown)
 
 params_list = list(
-  treatment = "all"
+  treatment = "all",
+  output_dir = "/run/user/1356082/gvfs/smb-share:server=group3.ad.helsinki.fi,share=h345/afarkkilab/Projects/NKI/Single_Cell_LR_Annalysis/output3/plots/"
 )
 
-render("3_Plot_generation.Rmd", params = params_list, output_dir = "/run/user/1356082/gvfs/smb-share:server=group3.ad.helsinki.fi,share=h345/afarkkilab/Projects/NKI/Single_Cell_LR_Annalysis/output/")
+render("3_Plot_generation.Rmd", params = params_list, output_dir = params_list$output_dir)
 
 # treatment : "chemo-naive" , IDS , all(for the whole dataset without filtering)
